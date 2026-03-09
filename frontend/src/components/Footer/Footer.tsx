372f708664dec632b834de94fb8c721bdb34d14d
AmandaPersdotter <amanda.vivianne.p.l@hotmail.com>
2026-03-09 13:24:29 +0100

    småfix css

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,11 +1,6 @@
 export default function Footer() {
   return (
-    <footer style={{ 
-      textAlign: "center", 
-      padding: "2rem 0",
-      fontSize: "0.8rem",
-      color: "#8f8f8f"
-    }}>
+    <footer className="footer">
       <a 
         href="https://www.flaticon.com/free-icons/cute" 
         title="cute icons"
